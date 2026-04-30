2182e4bffb2c7f15a1ea62dc1e7eeb72641e0b36
user <i1mk8.ln@gmail.com>
2026-04-30 09:53:42 +0500

    Скрывать документацию API, если debug выключен

--- backend/src/may_walk/main.py
+++ backend/src/may_walk/main.py
@@ -3,12 +3,23 @@
 from fastapi import FastAPI
 
 from may_walk.api.router import api_router
+from may_walk.core.settings import settings
 
 
 def create_app() -> FastAPI:
     """Создать и настроить FastAPI приложение."""
-    app = FastAPI(title='May Walk')
+    docs_url = '/docs' if settings.debug else None
+    redoc_url = '/redoc' if settings.debug else None
+    openapi_url = '/openapi.json' if settings.debug else None
+
+    app = FastAPI(
+        title='May Walk',
+        docs_url=docs_url,
+        redoc_url=redoc_url,
+        openapi_url=openapi_url,
+    )
     app.include_router(api_router)
+    
     return app
 
 
